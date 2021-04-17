182d7a0a4d56c351bbe9bad965a13c6dd3e4bcc4
Lasse =) <55485130+duriumm@users.noreply.github.com>
2021-04-17 19:03:40 +0200

    Dialogue system added with choosable options, based on TAGS, Popup marker with wound on entering npc interract circle

---
+++ Pixel Game/Assets/Scripts/DialogueViewer.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static DialogueObject;
+using UnityEngine.Events;
+using System;
+using System.Runtime.InteropServices;
+
+public class DialogueViewer : MonoBehaviour
+{
+    [SerializeField] Transform parentOfResponses;
+    [SerializeField] Button prefab_btnResponse;
+    [SerializeField] UnityEngine.UI.Text txtNodeDisplay;
+    [SerializeField] DialogueController dialogueController;
+    DialogueController controller;
+    private GameObject dialogueCanvas;
+    private GameObject dataToPassGameObject;
+
+    [DllImport("__Internal")]
+    private static extern void openPage(string url);
+
+    private const string questText = "QUEST";
+
+    private void Start()
+    {
+        controller = dialogueController;
+        controller.onEnteredNode += OnNodeEntered;
+        controller.InitializeDialogue();
+
+
+        // Start the dialogue
+        var curNode = controller.GetCurrentNode();
+
+        dialogueCanvas = this.gameObject;
+        dialogueCanvas.SetActive(false);
+        dataToPassGameObject = GameObject.FindGameObjectWithTag("PassData");
+    }
+
+    public static void KillAllChildren(UnityEngine.Transform parent)
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(parent);
+        for (int childIndex = parent.childCount - 1; childIndex >= 0; childIndex--)
+        {
+            UnityEngine.Object.Destroy(parent.GetChild(childIndex).gameObject);
+        }
+    }
+
+    private void OnNodeSelected(int indexChosen)
+    {
+        Debug.Log("Chose: " + indexChosen);
+        controller.ChooseResponse(indexChosen);
+    }
+
+    private void OnNodeEntered(Node newNode)
+    {
+        Debug.Log("Entering node: " + newNode.title);
+        txtNodeDisplay.text = newNode.text;
+
+        KillAllChildren(parentOfResponses);
+        for (int i = newNode.responses.Count - 1; i >= 0; i--)
+        {
+            int currentChoiceIndex = i;
+            var response = newNode.responses[i];
+            var responceButton = Instantiate(prefab_btnResponse, parentOfResponses);
+            responceButton.GetComponentInChildren<UnityEngine.UI.Text>().text = response.displayText;
+            responceButton.onClick.AddListener(delegate { OnNodeSelected(currentChoiceIndex); });
+        }
+
+        if (newNode.tags.Contains("END"))
+        {
+            // To-Do
+            // Here we can close the window or something :) since the talk is finished
+            Debug.Log("End!");
+        }
+        if (newNode.tags.Contains(questText))
+        {
+            // If the current node has a "QUEST" tag on it, we will take the current active npc from our database
+            // then find the GameObject with the same name in the scene and run ActivateQuest() on that NpcData script
+            Debug.Log("DE VA EN KVEEEST");
+
+            string currentActiveNpc = dataToPassGameObject.GetComponent<DataToPassBetweenScenes>().currentActivateNpc;
+            GameObject npcGameobject = GameObject.Find(currentActiveNpc);
+            npcGameobject.GetComponent<NpcData>().ActivateQuest();
+        }
+    }
+
+    // My own functions
+
+    // For turning the dialogue canvas on/off
+    public void SwitchDialogueCanvasOnOff()
+    {
+        if (dialogueCanvas.activeSelf == true) { dialogueCanvas.SetActive(false); }
+        else if (dialogueCanvas.activeSelf == false) { dialogueCanvas.SetActive(true); }
+
+    }
+}
---
+++ Pixel Game/Assets/Scripts/NpcData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcData : MonoBehaviour
+{
+    // Assign a twine convo fort this NPC
+    public TextAsset singleTwineConvo;
+    private GameObject dialogueController;
+    private GameObject dataToPassGameObject;
+
+    void Start()
+    {
+        dialogueController = GameObject.Find("DialogueController");
+        dataToPassGameObject = GameObject.FindGameObjectWithTag("PassData");
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "MyPlayer")
+        {
+            // Set this NPCs twine convo to be active in dialoguereader and then initialize the dialogue for the convo so it is loaded for next convo :)
+            dialogueController.GetComponent<DialogueController>().twineText = singleTwineConvo;
+            dialogueController.GetComponent<DialogueController>().InitializeDialogue();
+            dataToPassGameObject.GetComponent<DataToPassBetweenScenes>().currentActivateNpc = this.gameObject.name;
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "MyPlayer")
+        {
+            dataToPassGameObject.GetComponent<DataToPassBetweenScenes>().currentActivateNpc = "";
+        }
+    }
+
+    // To-Do - Activate a quest in a list of quests!
+    public void ActivateQuest()
+    {
+        Debug.Log("QUEST ACTIVATYED on npc named: "+ this.gameObject.name);
+    }
+}
