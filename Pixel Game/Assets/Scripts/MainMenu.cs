4d1a0fafd59f489b45555f394d2c102bfde4b989
Lasse =) <55485130+duriumm@users.noreply.github.com>
2021-06-22 17:55:47 +0200

    Cloud and frog animation added. Settings menu with ghast added. Working buttons for starting/exiting game and settings menu

---
+++ Pixel Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string sceneToLoad;
+    public Animator transition;
+    private bool isEasyMode = false;
+    private bool isAutoSaving = false;
+    private bool isSomethingEnabled = false;
+    public GameObject easyModeRedCross;
+    public GameObject autoSaveRedCross;
+    public GameObject somethingRedCross;
+    public AudioClip frogCatPigSound;
+    private GameObject mainCamera;
+
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+    }
+    public void PlayFrogCatPigSound()
+    {
+        AudioSource.PlayClipAtPoint(frogCatPigSound, mainCamera.transform.localPosition);
+    }
+    // These 3 functions below can be made into one function. But for now it will be like this.
+    // For now the functions only enable a red cross showing up but for future implementation
+    // these functions will enable or disable certain settings
+    public void ToggleEasyMode()
+    {
+        isEasyMode = !isEasyMode;
+        easyModeRedCross.SetActive(isEasyMode);
+        string enabledOrDisabled = isEasyMode ? "Enabled" : "Disabled";
+        Debug.Log("Easy mode is: " + enabledOrDisabled);
+    }
+    public void ToggleAutoSave()
+    {
+        isAutoSaving = !isAutoSaving;
+        autoSaveRedCross.SetActive(isAutoSaving);
+        string enabledOrDisabled = isAutoSaving ? "Enabled" : "Disabled";
+        Debug.Log("AutoSaving is: " + enabledOrDisabled);
+    }
+    public void ToggleSomething()
+    {
+        isSomethingEnabled = !isSomethingEnabled;
+        somethingRedCross.SetActive(isSomethingEnabled);
+        string enabledOrDisabled = isSomethingEnabled ? "Enabled" : "Disabled";
+        Debug.Log("Something button is: " + enabledOrDisabled);
+    }
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadLevel(sceneToLoad));
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game quit :), just not in the editor");
+    }
+
+    IEnumerator LoadLevel(string sceneToLoad)
+    {
+        transition.SetTrigger("Start");
+        // TO-DO - Disable player movement, Make player invincible
+
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Scene to load: " + sceneToLoad);
+        SceneManager.LoadScene(sceneToLoad);
+
+    }
+}
