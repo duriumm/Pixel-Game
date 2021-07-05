9383701805abe76051907866e7d381fc79ab5d3e
Lasse =) <55485130+duriumm@users.noreply.github.com>
2021-07-05 17:31:30 +0200

    Camera now follows player after starting game from main menu

--- Pixel Game/Assets/Scripts/DontDestroyTestScripts/CMvcam1DontDestroy.cs
+++ Pixel Game/Assets/Scripts/DontDestroyTestScripts/CMvcam1DontDestroy.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CMvcam1DontDestroy : MonoBehaviour
 {
     private static bool cMvcam1Exists;
+    private GameObject player;
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private string nameOfMainMenu = "Main_Menu_Lasse";
     void Start()
     {
+        if(SceneManager.GetActiveScene().name != nameOfMainMenu)
+        {
+            player = GameObject.FindGameObjectWithTag("MyPlayer");
+            cinemachineVirtualCamera = GameObject.FindGameObjectWithTag("VCam").GetComponent<CinemachineVirtualCamera>();
+
+            cinemachineVirtualCamera.Follow = player.transform;
+
+        }
+
         if (!cMvcam1Exists)
         {
             cMvcam1Exists = true;
             DontDestroyOnLoad(transform.gameObject);
+
+
+
+
+
         }
         else
         {
